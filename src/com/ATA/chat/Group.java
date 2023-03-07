79dfb1fddde4701ae4de393e0042a677f145a6e5
AnhTuyen1511 <tuyen.dothuy.cit19@eiu.edu.vn>
2023-03-07 14:20:04 +0700

    commit

--- src/com/ATA/chat/Group.java
+++ src/com/ATA/chat/Group.java
@@ -8,28 +8,34 @@ public abstract class Group {
 	private ArrayList<User> users;
 	private ArrayList<File> files;
 	private boolean isPrivate;
-	
+
 	public Group(String name) {
 		name = name;
 		this.users = new ArrayList<>();
 	}
+
 	public void addUser(User user) {
 		this.users.add(user);
 	}
-	public ArrayList<User> getUsers(){
+
+	public ArrayList<User> getUsers() {
 		return this.users;
 	}
+
 	public String getName() {
 		return name;
 	}
-	public ArrayList<File> getFiles(){
+
+	public ArrayList<File> getFiles() {
 		return this.files;
 	}
+
 	public boolean isPrivate() {
 		return isPrivate;
 	}
+
 	public void setPrivate(boolean isPrivate) {
 		this.isPrivate = isPrivate;
 	}
-	
+
 }
